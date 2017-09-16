25507781bf9d6b6cc7076af4675670f920c29d76
besermenji <besermenji.vladimir@gmail.com>
2017-09-16 10:51:08 +0200

    /login and /logout now routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   get 'hello_world', to: 'hello_world#index'
+  root 'hello_world#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
